0f2e351c660f8bde3b38e0d6811bc7a236946f6b
ironheart <utsavxtha1000@gmail.com>
2023-08-19 15:45:24 +0545

    try

---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const register = () => {
+  return (
+    <div>
+      <h1 className="text-white">WELCOME TO ARTVEDIC</h1>
+      <p className="text-white">
+        Delve into our feelings, sight and emotions through my artwork
+      </p>
+      <form></form>
+    </div>
+  );
+};
+
+export default register;
